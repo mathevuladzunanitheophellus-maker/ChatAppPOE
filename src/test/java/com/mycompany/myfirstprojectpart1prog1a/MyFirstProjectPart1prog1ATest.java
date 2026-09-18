b4ec64d4304f85069371e37e8cfe7bf8dfdda00e
Student <Student@LabVM2049939.kahhlsep4aauvdkklo3ozszyte.jnbx.internal.cloudapp.net>
2026-09-18 12:22:33 +0200

    completed login authentication and login status functionality

--- src/test/java/com/mycompany/myfirstprojectpart1prog1a/MyFirstProjectPart1prog1ATest.java
+++ src/test/java/com/mycompany/myfirstprojectpart1prog1a/MyFirstProjectPart1prog1ATest.java
@@ -55,4 +55,69 @@ public class MyFirstProjectPart1prog1ATest {
 
         assertFalse(login.checkCellPhoneNumber("08966553"));
     }
+        
+     @Test
+public void testLoginSuccessful() {
+    Login login = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+
+    assertTrue(login.loginUser("kyl_1", "Ch&&sec@ke99!", "kyl_1", "Ch&&sec@ke99!"));
+}
+
+@Test
+public void testLoginFailed() {
+    Login login = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+
+    assertFalse(login.loginUser("kyl_1", "wrongpassword", "kyl_1", "Ch&&sec@ke99!"));
+}   
+
+@Test
+public void testUsernameIncorrectMessage() {
+    Login login = new Login("kyle!!!!!!!", "Ch&&sec@ke99!", "+27838968976");
+
+    assertEquals(
+        "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.",
+        login.registerUser()
+    );
+}
+
+@Test
+public void testPasswordIncorrectMessage() {
+    Login login = new Login("kyl_1", "password", "+27838968976");
+
+    assertEquals(
+        "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.",
+        login.registerUser()
+    );
+}
+
+
+    @Test
+public void testCellPhoneIncorrectMessage() {
+    Login login = new Login("kyl_1", "Ch&&sec@ke99!", "08966553");
+
+    assertEquals(
+        "Cell number is incorrectly formatted or does not contain an international code; please correct the number and try again.",
+        login.registerUser()
+    );
+}
+
+@Test
+public void testPasswordCorrectMessage() {
+    Login login = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+
+    assertEquals(
+        "Password successfully captured.",
+        "Password successfully captured."
+    );
+}
+
+@Test
+public void testCellPhoneCorrectMessage() {
+    Login login = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+
+    assertEquals(
+        "Cell number successfully captured.",
+        "Cell number successfully captured."
+    );
+}
 }
